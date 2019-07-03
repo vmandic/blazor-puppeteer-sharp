f5656097cee9f62fca0580e29f3c3ce83e196130
Vedran Mandić <mandic.vedran@gmail.com>
2019-07-03 22:11:58 +0200

    init

---
+++ test/PuppeteerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PuppeteerConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Starting Puppeteer Sharp...");
+    }
+  }
+}
